7d6f66992948285c6feee661cc2b31b233f26795
Yaroslav19735 <yarkela1997@gmail.com>
2022-10-24 21:24:33 +0300

    correct

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player'; 
+// import throttle from 'lodash.throttle';
+var throttle = require('lodash.throttle'); 
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_TIME = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(playTime, 1000));
+
+lastTime(); //якщо localStorage заповнений тоді відтворення відео почнеться з останнього збереженого часу в ньому 
+
+function playTime(data) { //ця функція записує в localStorage час(секунди) відтворення
+    localStorage.setItem(STORAGE_TIME, data.seconds)
+}
+
+function lastTime() {//ця функція запише в localStorage секунди відтворення, а під час перезавантаження сторінки викличе на тому ж проміжку часу setCurrentTime
+    const setTime = localStorage.getItem(STORAGE_TIME);
+
+    if (setTime) {
+        player.setCurrentTime(setTime);
+    }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener("input", throttle(valueForm, 500));
+form.addEventListener("submit", onSubmit);
+
+let object = {};
+inputValues();
+function inputValues(event) {
+  if (!localStorage.getItem(STORAGE_KEY)) {
+    return;
+  }
+  object = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (object.email) {
+    form[0].value = object.email;
+  }
+  if (object.message) {
+    form[1].value = object.message;
+  }
+}
+
+function valueForm(event) {
+  object[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(object));
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  // object = {};
+  console.log(object);
+}
